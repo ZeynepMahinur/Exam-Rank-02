b0af81dc8ddb58a56f7360b149e2da2e3dc26d52
ZeynepMahinur <122391696+ZeynepMahinur@users.noreply.github.com>
2025-10-02 10:03:14 +0300

    Add files via upload

--- level4/ft_list_remove_if.c
+++ level4/ft_list_remove_if.c
@@ -15,5 +15,5 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
         ft_list_remove_if(begin_list, data_ref, cmp);
     }
     else
-        ft_list_remove_if(&((*begin_list)->next, data_ref, cmp));
+        ft_list_remove_if(&((*begin_list)->next), data_ref, cmp);
 }
--- level4/sort_list.c
+++ level4/sort_list.c
@@ -1,26 +1,14 @@
 #include <stdlib.h>
-#include "list.h"
-
-int ascending(int a, int b)
+typedef struct    s_list
 {
-	return (a <= b);
-}
+    struct s_list *next;
+    int            data;
+}                 t_list;
 
-t_list *new_node(int value)
-{
-    t_list *node = malloc(sizeof(t_list));
-    if (!node)
-        return (NULL);
-    node->data = value;
-    node->next = NULL;
-    return (node);
-}
-
-//ana fonksiyon lazım olan
 t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 {
-    int tmp;
     t_list *head;
+    int tmp;
 
     head = lst;
     if (!lst)
@@ -40,23 +28,28 @@ t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
     return (head);
 }
 
+int ascending(int a, int b)
+{
+	return (a <= b);
+}
+
 #include <stdio.h>
 int main()
 {
-    t_list *a = new_node(4);
-    t_list *b = new_node(1);
-    t_list *c = new_node(77);
-    t_list *d = new_node(10);
+    t_list a,b,c,d;
 
-    a->next = b;
-    b->next = c;
-    c->next = d;
-    t_list *sorted = sort_list(a, ascending);
-    t_list *tmp = sorted;
-    while (tmp)
+    a.data = 2, a.next = &b;
+    b.data = 1, b.next = &c;
+    c.data = 5, c.next = &d;
+    d.data = 0, d.next = NULL;
+
+    t_list *head;
+    head = &a;
+    head = sort_list(head, ascending);
+
+    while (head)
     {
-        printf("%d ", tmp->data);
-        tmp = tmp->next;
+        printf("%d ", head->data);
+        head = head->next;
     }
-    printf("\n");
 }
