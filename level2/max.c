653dcf4cdb0e5764c9c8fc70fb76ca754d3d04ae
ZeynepMahinur <122391696+ZeynepMahinur@users.noreply.github.com>
2025-10-03 09:24:35 +0300

    Update max.c

--- level2/max.c
+++ level2/max.c
@@ -1,15 +1,14 @@
 int		max(int* tab, unsigned int len)
 {
+    if (len == 0)
+        return(0);
     int i = 0;
-    int count = 0;
-    int result = 0;
-
-    while (count < len)
+    int result = tab[0];
+    while (i < len)
     {
         if (tab[i] > result)
             result = tab[i];
         i++;
-        count++;
     }
     return (result);
 }
